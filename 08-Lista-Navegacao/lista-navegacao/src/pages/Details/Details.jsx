3a322c64734a650e05ea8a64066634fa4ec4629b
Henrique <n.n.henrique.n.n@gmail.com>
2023-08-10 10:50:22 -0300

    Desestruturacao

--- 08-Lista-Navegacao/lista-navegacao/src/pages/Details/Details.jsx
+++ 08-Lista-Navegacao/lista-navegacao/src/pages/Details/Details.jsx
@@ -1,8 +1,50 @@
-function Details() {
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { usersData } from "../../components/Data/data.js";
+
+const Details = () => {
+  const parameters = useParams();
+
+  const [user, setUser] = useState(undefined);
+
+  useEffect(() => {
+    const { id } = parameters;
+    const result = usersData.find((element) => element.id === id);
+    if (!result) {
+      setUser(null);
+      return;
+    }
+    setUser(result);
+  }, [parameters]);
+
   return (
-    <>
-      <h1>Details</h1>
-    </>
+    <div>
+      <h1>Detalhes</h1>
+      {user === undefined && (
+        <div>
+          <p>Carregando...</p>
+        </div>
+      )}
+      {user === null && (
+        <div>
+          <p>Usuário não encontrado.</p>
+        </div>
+      )}
+      {user && (
+        <div>
+          <img
+            alt={user.image.alt}
+            src={user.image.src}
+            width="250px"
+            height="auto"
+          />
+          <h2>{user.name}</h2>
+          <p>{user.description}</p>
+        </div>
+      )}
+      <Link to="/">Voltar</Link>
+    </div>
   );
-}
+};
+
 export default Details;
